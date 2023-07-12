909648c4649ef1253490329224c8797839407855
vipink28 <vipinkalyan28@gmail.com>
2023-07-12 10:39:57 +0530

    tv slice created

--- src/features/tv/tvSlice.js
+++ src/features/tv/tvSlice.js
@@ -1,4 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { requests } from "../../utility/requests";
+import axios from '../../utility/axios';
 
 const initialState= {
     nfOriginals:{
@@ -8,13 +10,35 @@ const initialState= {
     } 
 }
 
+export const fetchNetflixOriginals = createAsyncThunk(
+    'tv/fetchNetflixOriginals',
+    async () => {
+        const response = await axios.get(requests.getNfOriginals);
+        return response.data
+    }
+)
+
 export const tvSlice = createSlice({
     name: "tv",
     initialState,
     reducers: {},
     extraReducers: (builder) => {
-
+        builder
+        .addCase(fetchNetflixOriginals.pending, (state, action)=>{
+            state.nfOriginals.status = 'loading';
+        })
+        .addCase(fetchNetflixOriginals.fulfilled, (state, action)=>{
+            state.nfOriginals.status = 'success';
+            state.nfOriginals.data = action.payload;
+        })
+        .addCase(fetchNetflixOriginals.rejected, (state, action)=>{
+            state.nfOriginals.status = 'failed';
+            state.nfOriginals.error = action.error;
+        } )
     }
 });
+
+export const nfOriginalsSelector = (state)=>state.tv.nfOriginals;
+
 export default tvSlice.reducer;
 
--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchNetflixOriginals, nfOriginalsSelector } from '../features/tv/tvSlice';
 
 function HomeScreen(props) {
+    const dispatch = useDispatch();
+    useEffect(()=>{
+        dispatch(fetchNetflixOriginals());
+    }, [])
+
+    const nfOriginals = useSelector(nfOriginalsSelector);
+    console.log(nfOriginals);
+
     return (
         <div>
             Home
