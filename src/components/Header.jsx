43afd87a8debc889f22b5a9ba954aed2a3f8a5fb
vipink28 <vipinkalyan28@gmail.com>
2023-07-13 10:33:40 +0530

    Header created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
 function Header(props) {
+    const { video } = props;
+    console.log(video);
     return (
         <div>
-            Header
+           <img className='w-100' src={`https://image.tmdb.org/t/p/original${video.backdrop_path}`} alt="" />
+           <h1>{video.name}</h1>
         </div>
     );
 }
--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -1,20 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchNetflixOriginals, nfOriginalsSelector } from '../features/tv/tvSlice';
+import Header from '../components/Header';
 
 function HomeScreen(props) {
+    const [randomIndex, setRandomIndex]=useState(null);
+
     const dispatch = useDispatch();
     useEffect(()=>{
         dispatch(fetchNetflixOriginals());
     }, [])
-
     const nfOriginals = useSelector(nfOriginalsSelector);
-    console.log(nfOriginals);
+
+    useEffect(()=>{
+        if(nfOriginals.status === "success"){
+            let randomIndex = Math.floor(Math.random() * nfOriginals.data.results.length);
+            setRandomIndex(randomIndex);
+        }
+    }, [nfOriginals])
+    
+
+
+
 
     return (
-        <div>
-            Home
-        </div>
+        <>
+            {
+                nfOriginals.status === "success" ?
+                <Header video={nfOriginals.data.results[randomIndex]}/>
+                : "loading"
+            }
+            
+        </>
+       
     );
 }
 
